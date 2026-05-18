b8f28444d0d4bb102055fa7fc9f791166ecd8a71
kotbk <kpas95545@gmail.com>
2026-05-19 01:32:04 +0400

    change simple test for CI/CD

--- habits/tests.py
+++ habits/tests.py
@@ -2,7 +2,12 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 class HabitTestCase(APITestCase):
-    def test_habit_list(self):
-        """ Тестируем, что эндпоинт привычек доступен """
-        response = self.client.get('/habits/')
+    def test_docs_available(self):
+        """ Проверяем доступность документации Swagger """
+        response = self.client.get('/api/docs/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_habits_list_endpoint(self):
+        """ Проверяем эндпоинт привычек 1 """
+        response = self.client.get('/api/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
